9353b8a6fa3e9415a69628870fb11b6c63eaacd5
kaligraphy247 <chikeziejames3@gmail.com>
2021-06-18 18:32:07 +0200

    allowed all incoming connections with *

--- mb_project/settings.py
+++ mb_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^-v=@nh65is(+tfjka*1wxjnyawcmc)c9j&gig5+1n%+^@%ot3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
